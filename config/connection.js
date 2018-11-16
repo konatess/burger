a0b29e499014c8b7ca8fc1b80a0338ca49f45e88
Kona Tess <konatess@gmail.com>
2018-11-15 23:59:13 -0800

    Added Heroku info to connection.js.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // DEPENDENCIES
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// declare variable
+var connection; 
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
   });
+};
 
-// MODULE FOR EXPORT
+connection.connect();
+// MODULE EXPORT
 module.exports = connection;
